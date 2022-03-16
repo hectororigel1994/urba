faa9c473be24eff0849d7d10b0a6cd602149cb49
hectororigel1994 <hector.origel1994@gmail.com>
2022-03-16 22:47:19 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -31,33 +31,3 @@ $(document).ready(function () {
 
     /*Alianzas*/
 
-/*Carousel*/
-/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
-$('.carousel').carousel({
-    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
-  });
-
-/*Testimonial*/
-$(document).ready(function () {
-    var silder = $(".owl-carousel");
-    silder.owlCarousel({
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: false,
-    items: 1,
-    stagePadding: 20,
-    center: true,
-    nav: false,
-    margin: 50,
-    dots: true,
-    loop: true,
-    responsive: {
-    0: { items: 1 },
-    480: { items: 2 },
-    575: { items: 2 },
-    768: { items: 2 },
-    991: { items: 3 },
-    1200: { items: 4 }
-    }
-    });
-    });
