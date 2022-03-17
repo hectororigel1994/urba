776739e38cb7d3384140e35b02871cb1ad4ba884
hectororigel1994 <hector.origel1994@gmail.com>
2022-03-17 02:15:06 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -29,5 +29,17 @@ $(document).ready(function () {
     });
     });
 
-    /*Alianzas*/
+    /* contact form */
+    $(document).ready(function(){
+
+        $('#smartwizard').smartWizard({
+        selected: 0,
+        theme: 'arrows',
+        autoAdjustHeight:true,
+        transitionEffect:'fade',
+        showStepURLhash: false,
+        
+        });
+        
+        });
 
