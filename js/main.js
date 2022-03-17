934504c868474163bc17cf782ae1c054fc2d4eca
hectororigel1994 <hector.origel1994@gmail.com>
2022-03-17 02:36:56 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -31,4 +31,8 @@ $(document).ready(function () {
 
     /* contact form */
    
+    $(document).ready(function(){
+        $('#birth-date').mask('00/00/0000');
+        $('#phone-number').mask('0000-0000');
+       })
 
